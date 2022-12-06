9c5e83849d1d7f75f9bc95b3ec0d196251cb8c98
Mercury0606 <mercuryzhong0606@gmail.com>
2022-12-06 18:09:24 +0800

    Selenium

---
+++ src/main/java/Java/Assignment.java
@@ -0,0 +1,24 @@
+package Java;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Assignment {
+    public static void main(String[] args) {
+        String text = "Rahul";
+        WebDriver driver = new ChromeDriver();
+         driver.get("https://www.rahulshettyacademy.com/AutomationPractice/");
+         driver.findElement(By.id("name")).sendKeys(text);
+         driver.findElement(By.id("alertbtn")).click();// To click on the Alert button
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().accept();//accept the alert of the browser instead of the content in the window.
+
+
+
+
+         driver.findElement(By.id("confirmbtn")).click();
+        System.out.println(driver.switchTo().alert().getText());
+        driver.switchTo().alert().dismiss();// To click on nagative element liek 'NO','Cancel',
+    }
+}
